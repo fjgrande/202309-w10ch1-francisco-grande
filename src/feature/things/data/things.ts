ae3e6577b39c4d747075896cea0231476c88c3bc
Francisco Grande <fjgrandv@gmail.com>
2023-11-13 23:35:18 +0100

    Add data things

---
+++ src/feature/things/data/things.ts
@@ -0,0 +1,26 @@
+import { type Thing } from "../../../types";
+
+const things: Thing[] = [
+  {
+    id: 1,
+    technology: "HTML",
+    description: "language",
+  },
+  {
+    id: 2,
+    technology: "CSS",
+    description: "language",
+  },
+  {
+    id: 3,
+    technology: "Javascript",
+    description: "language",
+  },
+  {
+    id: 4,
+    technology: "React",
+    description: "library",
+  },
+];
+
+export default things;
