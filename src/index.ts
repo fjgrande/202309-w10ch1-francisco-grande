c064199fdd1801300823c5fea9da15936a73a24c
Francisco Grande <fjgrandv@gmail.com>
2023-11-15 12:03:13 +0100

    Add routers in index

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,11 @@ import morgan from "morgan";
 import app from "./app.js";
 import { notFound } from "./middleware/error/errorMiddleware.js";
 import thingsRouter from "./feature/things/routers/thingsRouter.js";
+import PingController from "./feature/ping/controller/PingController.js";
+
+const pingController = new PingController();
 
 app.use(morgan("dev"));
+app.get("/", pingController.getPong);
 app.use("/things", thingsRouter);
 app.use(notFound);
